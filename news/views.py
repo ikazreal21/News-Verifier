835e6d8942f7748117f735753532f4976a9eab5b
ikazreal21 <66871228+ikazreal21@users.noreply.github.com>
2022-11-30 14:49:51 +0800

    second commit

--- news/views.py
+++ news/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
-# Create your views here.
+from .models import *
+from .forms import *
+
+def index(request):
+    context = {}
+    return render(request, 'news/index.html', context)
