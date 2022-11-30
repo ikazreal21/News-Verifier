5c17039651b73e78bd1d4dae86093e14b6f8c1d5
ikazreal21 <66871228+ikazreal21@users.noreply.github.com>
2022-11-30 14:35:37 +0800

    First Commit

---
+++ newVerifier/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for newVerifier project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'newVerifier.settings')
+
+application = get_asgi_application()
