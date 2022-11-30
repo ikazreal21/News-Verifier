ab53a0b4d7539ac2d8c65f615d28c1feb108eeb3
jbalmonte <joshua@packworks.io>
2022-11-30 22:38:54 +0800

    some UI updates

--- Fake_News_Det.py
+++ Fake_News_Det.py
@@ -1,4 +1,3 @@
-from flask import Flask, render_template, request
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import PassiveAggressiveClassifier
 import pickle
@@ -6,12 +5,15 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 
 app = Flask(__name__)
-tfvect = TfidfVectorizer(stop_words='english', max_df=0.7)
-loaded_model = pickle.load(open('model.pkl', 'rb'))
-dataframe = pd.read_csv('news.csv')
-x = dataframe['text']
-y = dataframe['label']
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
+tfvect = TfidfVectorizer(stop_words="english", max_df=0.7)
+loaded_model = pickle.load(open("model.pkl", "rb"))
+dataframe = pd.read_csv("news.csv")
+x = dataframe["text"]
+y = dataframe["label"]
+x_train, x_test, y_train, y_test = train_test_split(
+    x, y, test_size=0.2, random_state=0
+)
+
 
 def fake_news_det(news):
     tfid_x_train = tfvect.fit_transform(x_train)
@@ -21,19 +23,22 @@ def fake_news_det(news):
     prediction = loaded_model.predict(vectorized_input_data)
     return prediction
 
-@app.route('/')
-def home():
-    return render_template('index.html')
 
-@app.route('/predict', methods=['POST'])
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/predict", methods=["POST"])
 def predict():
-    if request.method == 'POST':
-        message = request.form['message']
+    if request.method == "POST":
+        message = request.form["message"]
         pred = fake_news_det(message)
         print(pred)
-        return render_template('index.html', prediction=pred)
+        return render_template("index.html", prediction=pred)
     else:
-        return render_template('index.html', prediction="Something went wrong")
+        return render_template("index.html", prediction="Something went wrong")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
