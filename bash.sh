6d3965214f010fbd0fe7bd8aba2012109228bd34
ikazreal21 <66871228+ikazreal21@users.noreply.github.com>
2022-12-04 00:35:26 +0800

    development

--- bash.sh
+++ bash.sh
@@ -2,7 +2,11 @@
 # exit on error
 set -o errexit
 
+pip install --upgrade pip
+pip install --force-reinstall -U setuptools
+
 poetry install
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
