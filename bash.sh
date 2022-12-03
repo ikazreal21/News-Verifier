4fbdda4c20da18d14b6e2b0bbccef56e5055faf7
ikazreal21 <66871228+ikazreal21@users.noreply.github.com>
2022-12-04 00:41:46 +0800

    deployment

--- bash.sh
+++ bash.sh
@@ -2,11 +2,10 @@
 # exit on error
 set -o errexit
 
-pip install --upgrade pip
-pip install --force-reinstall -U setuptools
 
 poetry install
-
+pip install --upgrade pip
+pip install --force-reinstall -U setuptools
 python manage.py collectstatic --no-input
 python manage.py migrate
 
