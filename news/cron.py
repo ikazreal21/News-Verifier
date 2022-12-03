0d0db5eb2cf32ac95d35177804f3716c97a80306
ikazreal21 <66871228+ikazreal21@users.noreply.github.com>
2022-12-03 14:36:28 +0800

    update

--- news/cron.py
+++ news/cron.py
@@ -2,4 +2,4 @@ from datetime import datetime, timedelta
 from .models import *
 
 def check_expired_data():
-    News.objects.filter(posting_date__lte=datetime.now()-timedelta(days=7)).delete()
+    News.objects.filter(posting_date__lte=datetime.now()-timedelta(days=30)).delete()
